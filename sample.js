aee568b0f7cbf68567c24906a543a6ec62db7259
elias <eltes2010@gmail.com>
2025-03-01 14:26:47 +0100

    remove item from array

---
+++ sample.js
@@ -0,0 +1,22 @@
+const names = [
+    "Peter",
+    "Ahmad",
+    "Yana",
+    "kristina",
+    "Rasmus",
+    "Samuel",
+    "katrine",
+    "Tala",
+];
+const nameToRemove = "Ahmad";
+
+// Write some code here
+// find the index of the name that needs to be removed
+const index = names.indexOf(nameToRemove)
+
+// Code done
+if(index !== -1){
+    names.splice(index,1)
+}
+
+console.log(names); // ['Peter', 'Yana', 'kristina', 'Rasmus', 'Samuel', 'katrine', 'Tala']
